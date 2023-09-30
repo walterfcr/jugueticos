0f8ac8b484400ad1ce110ae616c4a84f6c87a3e8
walterfcr <wfallasb@ucenfotec.ac.cr>
2023-09-30 16:29:12 -0600

    agregando archivos

---
+++ js/juguetes.js
@@ -0,0 +1,27 @@
+function juguete1(){
+    document.getElementById("juguete1").style.display = 'block';
+    document.getElementById("juguete2").style.display = 'none';
+    document.getElementById("juguete3").style.display = 'none';
+    document.getElementById("juguete4").style.display = 'none';
+}
+
+function juguete2(){
+    document.getElementById("juguete1").style.display = 'none';
+    document.getElementById("juguete2").style.display = 'block';
+    document.getElementById("juguete3").style.display = 'none';
+    document.getElementById("juguete4").style.display = 'none';
+}
+
+function juguete3(){
+    document.getElementById("juguete1").style.display = 'none';
+    document.getElementById("juguete2").style.display = 'none';
+    document.getElementById("juguete3").style.display = 'block';
+    document.getElementById("juguete4").style.display = 'none';
+}
+
+function juguete4(){
+    document.getElementById("juguete1").style.display = 'none';
+    document.getElementById("juguete2").style.display = 'none';
+    document.getElementById("juguete3").style.display = 'none';
+    document.getElementById("juguete4").style.display = 'block';
+}
